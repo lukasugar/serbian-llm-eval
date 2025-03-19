bd874a2eec2f897695f82d5a6e5b7d18913c042a
lukasugar <lukasugar@gmail.com>
2025-03-19 13:13:40 +0100

    install wandb

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setuptools.setup(
         "transformers>=4.1",
         "zstandard",
         "accelerate>=0.17.1",
+        "wandb", # for logging to wandb
     ],
     extras_require={
         "dev": ["black", "flake8", "pre-commit", "pytest", "pytest-cov"],
